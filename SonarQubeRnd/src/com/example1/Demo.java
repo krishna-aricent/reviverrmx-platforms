26590f408868bef8548eab97de38a2dae538cc66
Krishna Kumar <krishna4.kumar@aricent.com>
2017-12-07 11:33:44 +0530

    java code added for SonarQube test

---
+++ SonarQubeRnd/src/com/example1/Demo.java
@@ -0,0 +1,11 @@
+package com.example1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("In the main method of Demo");
+	}
+
+}
